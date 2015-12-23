2926741b37b9c9dbbc48cfc8e5f2806489175981
ggj2010 <gaoguangjin>
2015-12-23 12:28:09 +0800

    【a】:修改springboot端口号

---
+++ src/main/java/com/ggj/springboot/bean/Message.java
@@ -0,0 +1,17 @@
+package com.ggj.springboot.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * author:gaoguangjin
+ * Description:lombok 在idea里面直接搜索这个插件就可以使用了
+ * Email:335424093@qq.com
+ * Date 2015/12/23 11:24
+ */
+@Setter
+@Getter
+public class Message {
+    private String name;
+    private String content;
+}
