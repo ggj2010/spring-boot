c671c76f65ec9591cadf1089c5b89bc9b2e794c9
ggj2010 <gaoguangjin>
2016-03-28 14:28:26 +0800

    spring boot  develop tool功能

--- src/main/java/com/ggj/springboot/controller/IndexController.java
+++ src/main/java/com/ggj/springboot/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.ggj.springboot.bean.Student;
 import com.ggj.springboot.database.h2.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class IndexController {
     @Autowired
     private StudentService studentService;
+
 
     private AtomicInteger atomicInteger = new AtomicInteger();
 
